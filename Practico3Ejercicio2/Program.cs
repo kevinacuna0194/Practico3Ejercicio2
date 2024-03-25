8f3177bbae4f0360190695dd44f99814e39d3bcd
Kevin Acuña <94419050+kevinacuna0194@users.noreply.github.com>
2024-03-25 17:18:30 -0300

    Agregar archivos de proyecto.

---
+++ Practico3Ejercicio2/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Practico3Ejercicio2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
